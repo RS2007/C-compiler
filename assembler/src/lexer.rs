73e2bfc0c529452ba356f8d169e01adaccb32824
Rohith <rohithsuresh207@gmail.com>
2024-04-13 19:45:33 +0530

    fix+clean: whitespace bug + removing unnecessary code

--- assembler/src/lexer.rs
+++ assembler/src/lexer.rs
@@ -98,7 +98,7 @@ fn omit_whitespace(iter: &mut Peekable<Chars<'static>>, col: &mut u64, line: &mu
 impl Lexer {
     pub fn new(input: &'static str) -> Self {
         return Lexer {
-            input,
+            input: input.trim(),
             tokens: vec![],
         };
     }
@@ -108,7 +108,6 @@ impl Lexer {
         let mut line = 1;
         let mut col = 1;
         'lex_loop: while let Some(&character) = input_iter.peek() {
-            omit_whitespace(&mut input_iter, &mut col, &mut line);
             match character {
                 ',' => {
                     let token = Token {
@@ -153,83 +152,63 @@ impl Lexer {
                 'a'..='z' | 'A'..='Z' => {
                     let prev_col = col.clone();
                     let identifier = lex_identifier(&mut input_iter, &mut col);
-                    // TODO: hashmap might be better for speed(with maybe a custom hash function)
-                    let special_identifiers = vec![
-                        "add", "sub", "mul", "div", "cmp", "mov", "ldr", "str", "stp", "b", "bl",
-                        "cbz", "cbnz", "asr", "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8",
-                        "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19",
-                        "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29",
-                        "x30", "sp", "xzr",
-                    ];
-
-                    if special_identifiers.contains(&&*identifier.as_str()) {
-                        special_identifiers.iter().for_each(|&inst| {
-                            if inst == *identifier {
-                                let token = Token {
-                                    token_type: match inst {
-                                        "add" => TokenType::AddInst,
-                                        "sub" => TokenType::SubInst,
-                                        "mul" => TokenType::MulInst,
-                                        "div" => TokenType::DivInst,
-                                        "cmp" => TokenType::CmpInst,
-                                        "mov" => TokenType::MovInst,
-                                        "ldr" => TokenType::LdrInst,
-                                        "str" => TokenType::StrInst,
-                                        "stp" => TokenType::StpInst,
-                                        "b" => TokenType::BInst,
-                                        "bl" => TokenType::BlInst,
-                                        "cbz" => TokenType::CbzInst,
-                                        "cbnz" => TokenType::CbnzInst,
-                                        "asr" => TokenType::AsrInst,
-                                        "x0" => TokenType::Register(Box::new("x0".to_string())),
-                                        "x1" => TokenType::Register(Box::new("x1".to_string())),
-                                        "x2" => TokenType::Register(Box::new("x2".to_string())),
-                                        "x3" => TokenType::Register(Box::new("x3".to_string())),
-                                        "x4" => TokenType::Register(Box::new("x4".to_string())),
-                                        "x5" => TokenType::Register(Box::new("x5".to_string())),
-                                        "x6" => TokenType::Register(Box::new("x6".to_string())),
-                                        "x7" => TokenType::Register(Box::new("x7".to_string())),
-                                        "x8" => TokenType::Register(Box::new("x8".to_string())),
-                                        "x9" => TokenType::Register(Box::new("x9".to_string())),
-                                        "x10" => TokenType::Register(Box::new("x10".to_string())),
-                                        "x11" => TokenType::Register(Box::new("x11".to_string())),
-                                        "x12" => TokenType::Register(Box::new("x12".to_string())),
-                                        "x13" => TokenType::Register(Box::new("x13".to_string())),
-                                        "x14" => TokenType::Register(Box::new("x14".to_string())),
-                                        "x15" => TokenType::Register(Box::new("x15".to_string())),
-                                        "x16" => TokenType::Register(Box::new("x16".to_string())),
-                                        "x17" => TokenType::Register(Box::new("x17".to_string())),
-                                        "x18" => TokenType::Register(Box::new("x18".to_string())),
-                                        "x19" => TokenType::Register(Box::new("x19".to_string())),
-                                        "x20" => TokenType::Register(Box::new("x20".to_string())),
-                                        "x21" => TokenType::Register(Box::new("x21".to_string())),
-                                        "x22" => TokenType::Register(Box::new("x22".to_string())),
-                                        "x23" => TokenType::Register(Box::new("x23".to_string())),
-                                        "x24" => TokenType::Register(Box::new("x24".to_string())),
-                                        "x25" => TokenType::Register(Box::new("x25".to_string())),
-                                        "x26" => TokenType::Register(Box::new("x26".to_string())),
-                                        "x27" => TokenType::Register(Box::new("x27".to_string())),
-                                        "x28" => TokenType::Register(Box::new("x28".to_string())),
-                                        "x29" => TokenType::Register(Box::new("x29".to_string())),
-                                        "x30" => TokenType::Register(Box::new("x30".to_string())),
-                                        "sp" => TokenType::Register(Box::new("sp".to_string())),
-                                        "xzr" => TokenType::Register(Box::new("xzr".to_string())),
-                                        // TODO: is there a better way to structure this to avoid cloning?
-                                        _ => unreachable!(),
-                                    },
-                                    line,
-                                    col: prev_col,
-                                };
-                                self.tokens.push(token);
-                            }
-                        });
-                        continue 'lex_loop;
+                    let token_type = match identifier.as_str() {
+                        "add" => TokenType::AddInst,
+                        "sub" => TokenType::SubInst,
+                        "mul" => TokenType::MulInst,
+                        "div" => TokenType::DivInst,
+                        "cmp" => TokenType::CmpInst,
+                        "mov" => TokenType::MovInst,
+                        "ldr" => TokenType::LdrInst,
+                        "str" => TokenType::StrInst,
+                        "stp" => TokenType::StpInst,
+                        "b" => TokenType::BInst,
+                        "bl" => TokenType::BlInst,
+                        "cbz" => TokenType::CbzInst,
+                        "cbnz" => TokenType::CbnzInst,
+                        "asr" => TokenType::AsrInst,
+                        "x0" => TokenType::Register(Box::new("x0".to_string())),
+                        "x1" => TokenType::Register(Box::new("x1".to_string())),
+                        "x2" => TokenType::Register(Box::new("x2".to_string())),
+                        "x3" => TokenType::Register(Box::new("x3".to_string())),
+                        "x4" => TokenType::Register(Box::new("x4".to_string())),
+                        "x5" => TokenType::Register(Box::new("x5".to_string())),
+                        "x6" => TokenType::Register(Box::new("x6".to_string())),
+                        "x7" => TokenType::Register(Box::new("x7".to_string())),
+                        "x8" => TokenType::Register(Box::new("x8".to_string())),
+                        "x9" => TokenType::Register(Box::new("x9".to_string())),
+                        "x10" => TokenType::Register(Box::new("x10".to_string())),
+                        "x11" => TokenType::Register(Box::new("x11".to_string())),
+                        "x12" => TokenType::Register(Box::new("x12".to_string())),
+                        "x13" => TokenType::Register(Box::new("x13".to_string())),
+                        "x14" => TokenType::Register(Box::new("x14".to_string())),
+                        "x15" => TokenType::Register(Box::new("x15".to_string())),
+                        "x16" => TokenType::Register(Box::new("x16".to_string())),
+                        "x17" => TokenType::Register(Box::new("x17".to_string())),
+                        "x18" => TokenType::Register(Box::new("x18".to_string())),
+                        "x19" => TokenType::Register(Box::new("x19".to_string())),
+                        "x20" => TokenType::Register(Box::new("x20".to_string())),
+                        "x21" => TokenType::Register(Box::new("x21".to_string())),
+                        "x22" => TokenType::Register(Box::new("x22".to_string())),
+                        "x23" => TokenType::Register(Box::new("x23".to_string())),
+                        "x24" => TokenType::Register(Box::new("x24".to_string())),
+                        "x25" => TokenType::Register(Box::new("x25".to_string())),
+                        "x26" => TokenType::Register(Box::new("x26".to_string())),
+                        "x27" => TokenType::Register(Box::new("x27".to_string())),
+                        "x28" => TokenType::Register(Box::new("x28".to_string())),
+                        "x29" => TokenType::Register(Box::new("x29".to_string())),
+                        "x30" => TokenType::Register(Box::new("x30".to_string())),
+                        "sp" => TokenType::Register(Box::new("sp".to_string())),
+                        "xzr" => TokenType::Register(Box::new("xzr".to_string())),
+                        // TODO: is there a better way to structure this to avoid cloning?
+                        _ => TokenType::Identifier(identifier),
                     };
-                    self.tokens.push(Token {
-                        token_type: TokenType::Identifier(identifier),
+                    let token = Token {
+                        token_type,
                         line,
                         col: prev_col,
-                    });
+                    };
+                    self.tokens.push(token);
                 }
                 '0'..='9' | '-' => {
                     let prev_col = col.clone();
@@ -243,6 +222,7 @@ impl Lexer {
                 }
                 _ => {}
             }
+            omit_whitespace(&mut input_iter, &mut col, &mut line);
         }
     }
 }
